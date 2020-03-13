19856d726c511c3b0fef2ac7183a18789375321e
hengkyr <hengky@femaledaily.com>
2020-03-13 23:22:49 +0700

    Modified RecycerView and Movie transfer data

--- app/src/main/java/com/example/mymoviecatalogue/MovieAdapter.kt
+++ app/src/main/java/com/example/mymoviecatalogue/MovieAdapter.kt
@@ -1,44 +1,47 @@
 package com.example.mymoviecatalogue
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.item_movies.view.*
 
-class MovieAdapter internal constructor(private val context: Context) : BaseAdapter(){
-    internal var movies = arrayListOf<Movie>()
 
-    override fun getView(position: Int, view: View?, viewGroup: ViewGroup?): View {
-        var itemView = view
-        if (itemView == null){
-            itemView = LayoutInflater.from(context).inflate(R.layout.item_movies, viewGroup, false)
-        }
+class MovieAdapter(private val listMovie: ArrayList<Movie>): RecyclerView.Adapter<MovieAdapter.ListViewHolder>() {
 
-        val viewHolder = ViewHolder(itemView as View)
+    var listener: OnMovieListener? = null
 
-        val movie = getItem(position) as Movie
-        viewHolder.bind(movie)
-        return itemView
-    }
+    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(movie: Movie) {
+            with(itemView){
+                Glide.with(itemView.context)
+                    .load(movie.photo)
+                    .into(img_photo)
 
-    override fun getItem(i: Int): Any = movies[i]
-
-    override fun getItemId(i: Int): Long = i.toLong()
-
-    override fun getCount(): Int = movies.size
-
-    private inner class ViewHolder internal constructor(view: View){
-        private val txtName: TextView = view.findViewById(R.id.txt_name)
-        private val txtDescription: TextView = view.findViewById(R.id.txt_description)
-        private val imgPhoto: ImageView = view.findViewById(R.id.img_photo)
-
-        internal fun bind(movie: Movie){
-            txtName.text = movie.name
-            txtDescription.text = movie.description
-            imgPhoto.setImageResource(movie.photo)
+                txt_name.text = movie.name
+                txt_description.text = movie.description
+                itemView.setOnClickListener {
+                    listener?.onMovieItemClicked(movie)
+                }
+            }
         }
     }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ListViewHolder {
+        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_movies, viewGroup, false)
+        return ListViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = listMovie.size
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        holder.bind(listMovie[position])
+    }
+
+    interface OnMovieListener{
+        fun onMovieItemClicked(data: Movie)
+    }
+
 }
--- app/src/main/java/com/example/mymoviecatalogue/MovieFragment.kt
+++ app/src/main/java/com/example/mymoviecatalogue/MovieFragment.kt
@@ -7,16 +7,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_movie.*
 
-/**
- * A simple [Fragment] subclass.
- */
-class MovieFragment : Fragment(), View.OnClickListener {
+class MovieFragment : Fragment(), View.OnClickListener, MovieAdapter.OnMovieListener {
 
-    private lateinit var adapter: MovieAdapter
+    private val list = ArrayList<Movie>()
 
     private lateinit var dataName: Array<String>
     private lateinit var dataDescription: Array<String>
@@ -25,7 +21,6 @@ class MovieFragment : Fragment(), View.OnClickListener {
     private lateinit var dataDirector: Array<String>
     private lateinit var dataRating: Array<String>
     private lateinit var dataPhoto: TypedArray
-    private var movies = ArrayList<Movie>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,36 +32,15 @@ class MovieFragment : Fragment(), View.OnClickListener {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        adapter = MovieAdapter(requireContext())
-        lv_movie.adapter = adapter
+        rv_movie.setHasFixedSize(true)
 
-        prepare()
-        addItem()
-
-        lv_movie.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-
-            val listDataMovie = Movie(0,"","","","","","")
-            listDataMovie.photo = dataPhoto.getResourceId(position, position)
-            listDataMovie.name = dataName[position]
-            listDataMovie.description = dataDescription[position]
-            listDataMovie.genre = dataGenre[position]
-            listDataMovie.duration = dataDuration[position]
-            listDataMovie.director = dataDirector[position]
-            listDataMovie.rating = dataRating[position]
+        list.addAll(getListMovie())
+        showRecyclerList()
 
 
-            val intent = Intent(activity, DetailMovie::class.java)
-            intent.putExtra(DetailMovie.EXTRA_MOVIE, listDataMovie)
-
-            this@MovieFragment.startActivity(intent)
-            Toast.makeText(activity, movies[position].name, Toast.LENGTH_SHORT).show()
-        }
-//        context?.let {context ->
-//
-//        }
     }
 
-    private fun prepare() {
+    private fun getListMovie(): ArrayList<Movie> {
         dataName = resources.getStringArray(R.array.data_name)
         dataDescription = resources.getStringArray(R.array.data_description)
         dataPhoto = resources.obtainTypedArray(R.array.data_photo)
@@ -74,9 +48,8 @@ class MovieFragment : Fragment(), View.OnClickListener {
         dataDuration = resources.getStringArray(R.array.data_duration)
         dataDirector = resources.getStringArray(R.array.data_director)
         dataRating = resources.getStringArray(R.array.data_rating)
-    }
 
-    private fun addItem() {
+        val listMovie = ArrayList<Movie>()
         for (position in dataName.indices) {
             val movie = Movie(
                 dataPhoto.getResourceId(position, -1),
@@ -87,13 +60,24 @@ class MovieFragment : Fragment(), View.OnClickListener {
                 dataDirector[position],
                 dataRating[position]
             )
-            movies.add(movie)
+            listMovie.add(movie)
         }
-        adapter.movies = movies
+        return listMovie
+    }
+
+    private fun showRecyclerList() {
+        rv_movie.layoutManager = LinearLayoutManager(requireContext())
+        val listMovieAdapter = MovieAdapter(list)
+        listMovieAdapter.listener = this
+        rv_movie.adapter = listMovieAdapter
+    }
+
+    override fun onMovieItemClicked(data: Movie) {
+        val intent = Intent(requireContext(), DetailMovie::class.java)
+        intent.putExtra(DetailMovie.EXTRA_MOVIE,data)
+        startActivity(intent)
     }
 
     override fun onClick(v: View?) {
-
     }
-
 }
